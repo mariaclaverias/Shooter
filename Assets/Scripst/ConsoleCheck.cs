7db6389ee019f9b6dac9e6a1a468f5fb3bd4bac0
AdelinaAscona <juana.ascona@tecsup.edu.pe>
2024-06-20 17:43:34 -0500

    Corrección_consola

--- Assets/Scripst/ConsoleCheck.cs
+++ Assets/Scripst/ConsoleCheck.cs
@@ -17,25 +17,44 @@ public class ConsoleCheck : MonoBehaviour
     {
 #if UNITY_EDITOR
         UnityEditor.BuildTarget buildTarget = UnityEditor.EditorUserBuildSettings.activeBuildTarget;
+        Debug.Log("Running in Unity Editor with build target: " + buildTarget);
+
         if (buildTarget == UnityEditor.BuildTarget.StandaloneWindows ||
             buildTarget == UnityEditor.BuildTarget.StandaloneWindows64)
         {
+            Debug.Log("Detected build target as Standalone Windows.");
             pcText.gameObject.SetActive(true);
+        }
+        else if (buildTarget == UnityEditor.BuildTarget.XboxOne ||
+                 buildTarget == UnityEditor.BuildTarget.WSAPlayer)
+        {
+            Debug.Log("Detected build target as Xbox or UWP.");
+            xboxText.gameObject.SetActive(true);
         }
         else
         {
-            xboxText.gameObject.SetActive(true);
+            Debug.Log("Detected build target as something else.");
         }
 #else
+        Debug.Log("Running outside of Unity Editor on platform: " + Application.platform);
+
         if (Application.platform == RuntimePlatform.WindowsPlayer ||
             Application.platform == RuntimePlatform.WindowsEditor)
         {
+            Debug.Log("Detected runtime platform as Windows.");
             pcText.gameObject.SetActive(true);
         }
         else if (Application.platform == RuntimePlatform.XboxOne ||
-                 Application.platform == RuntimePlatform.XboxSeriesX)
+                 Application.platform == RuntimePlatform.WSAPlayerX86 ||
+                 Application.platform == RuntimePlatform.WSAPlayerX64 ||
+                 Application.platform == RuntimePlatform.WSAPlayerARM)
         {
+            Debug.Log("Detected runtime platform as Xbox or UWP.");
             xboxText.gameObject.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Detected runtime platform as something else.");
         }
 #endif
     }
